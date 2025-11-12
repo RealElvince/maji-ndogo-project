d5a7154eafdbf36b09ae5a368043c424b87ed60f
RealElvince <obuongely@yahoo.com>
2025-11-12 19:28:50 +0300

    created location table

---
+++ db/location.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `location` (
+  `location_id` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `province_name` varchar(255) DEFAULT NULL,
+  `town_name` varchar(255) DEFAULT NULL,
+  `location_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+)
