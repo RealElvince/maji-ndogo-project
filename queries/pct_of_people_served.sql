18921a61889e0a3f241f4c228a10a264a46f56b6
RealElvince <obuongely@yahoo.com>
2025-11-19 22:03:58 +0300

    code refactor:percent of pple served in DESC

--- queries/pct_of_people_served.sql
+++ queries/pct_of_people_served.sql
@@ -1,10 +1,10 @@
 SELECT
 	type_of_water_source,
-	ROUND(SUM(number_of_people_served)/(27628140)*100,2) AS pct_of_people_served
+	ROUND(SUM(number_of_people_served)/(27628140)*100,0) AS pct_of_people_served
 FROM
 	water_source
 
 GROUP BY
 	type_of_water_source
 ORDER BY
-	type_of_water_source;
+	pct_of_people_served DESC;
