e250c71dc4555fdb4e57190e0018e1eab1a2067c
RealElvince <obuongely@yahoo.com>
2025-11-28 08:21:53 +0300

    added subjective quality score

--- audits/join_audit_report_visits_tables.sql
+++ audits/join_audit_report_visits_tables.sql
@@ -2,11 +2,17 @@ SELECT
 	ar.location_id AS audition_location,
     ar.true_water_source_score,
     v.location_id AS visit_location,
-    v.record_id
+    v.record_id,
+    wq.subjective_quality_score
 FROM
 	auditor_report ar
 
 INNER JOIN
 	visits v
 ON
-	ar.location_id = v.location_id;
+	ar.location_id = v.location_id
+
+INNER JOIN 
+	water_quality wq
+ON
+	v.record_id = wq.record_id;
