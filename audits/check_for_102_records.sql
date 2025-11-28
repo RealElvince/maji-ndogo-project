ae127125a15d99c4ef022e4ecfc63248424ede8d
RealElvince <obuongely@yahoo.com>
2025-11-28 08:58:09 +0300

    query to view 102 records

---
+++ audits/check_for_102_records.sql
@@ -0,0 +1,22 @@
+SELECT
+	ar.location_id AS location_id,
+    v.record_id,
+    ar.true_water_source_score AS auditor_score,
+    wq.subjective_quality_score AS surveyor_score
+FROM
+	auditor_report ar
+
+INNER JOIN
+	visits v
+ON
+	ar.location_id = v.location_id
+
+INNER JOIN 
+	water_quality wq
+ON
+	v.record_id = wq.record_id
+    
+WHERE
+	v.visit_count = 1
+AND
+	ar.true_water_source_score - wq.subjective_quality_score <> 0;
